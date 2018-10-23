c3b758c3af20658ce89bc55fb33b0a9cdd76facd
Gonçalo Miguel Monteiro Ferreira Barras Lobo <fc44870@alunos.fc.ul.pt>
2018-10-23 17:25:40 +0100

    Tentativa Barra
    Push da barra

--- androidProjects - copia/app/src/main/java/com/example/android/activitygo/AchievementsFragment.java
+++ androidProjects - copia/app/src/main/java/com/example/android/activitygo/AchievementsFragment.java
@@ -3,6 +3,8 @@ package com.example.android.activitygo;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,4 +26,12 @@ public class AchievementsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_achievements, container, false);
     }
+
+    /*
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.menu_main,menu);
+        super.onCreateOptionsMenu(menu,menuInflater);
+    }
+    */
 }
