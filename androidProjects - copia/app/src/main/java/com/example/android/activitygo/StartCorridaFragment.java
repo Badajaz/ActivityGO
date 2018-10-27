2c5ba0198d25054f2ec95ca5e43ffe0c93c13505
fc46784 <fc46784@alunos.fc.ul.pt>
2018-10-27 16:25:58 +0100

    cronometro implementado
    cronometro a funcionar

--- androidProjects - copia/app/src/main/java/com/example/android/activitygo/StartCorridaFragment.java
+++ androidProjects - copia/app/src/main/java/com/example/android/activitygo/StartCorridaFragment.java
@@ -5,11 +5,13 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.SystemClock;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.Toast;
 
 
@@ -17,6 +19,13 @@ import android.widget.Toast;
  * A simple {@link Fragment} subclass.
  */
 public class StartCorridaFragment extends Fragment {
+
+
+    private Chronometer chronometer;
+    private boolean running;
+    private long pauseOffset;
+
+
 
     private Fragment SelectedFragment;
     public StartCorridaFragment() {
@@ -30,17 +39,38 @@ public class StartCorridaFragment extends Fragment {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_start_corrida, container, false);
         Button Stop = (Button) v.findViewById(R.id.StopRun);
+        Button Start = (Button) v.findViewById(R.id.StartRun);
+        chronometer = v.findViewById(R.id.chronometer);
+
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!running){
+                    chronometer.setBase(SystemClock.elapsedRealtime()- pauseOffset);
+                    chronometer.start();
+                    running = true;
+                }
+            }
+        });
 
         Stop.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                SelectedFragment = new RunFragment();
+
+                if (running){
+                    pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                    chronometer.stop();
+                    running = false ;
+                }
+
+                /*SelectedFragment = new RunFragment();
                 FragmentManager fmana = getFragmentManager();
                 FragmentTransaction ftransacti = fmana.beginTransaction();
                 ftransacti.replace(R.id.fragment_container, SelectedFragment,"StartCorridaFragment");
                 ftransacti.addToBackStack("IrCorridaFragment");
-                ftransacti.commit();
+                ftransacti.commit();*/
             }
         });
 
