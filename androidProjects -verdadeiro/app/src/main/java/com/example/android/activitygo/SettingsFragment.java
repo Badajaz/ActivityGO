57d0a64a900bd28273b28c9954b1127abeb82455
goncaloLobo <g.ferreiralobo@gmail.com>
2018-11-02 11:37:15 +0000

    Spinner Contactos Importantes
    Ajustar textAlignment e background do spinner

--- androidProjects -verdadeiro/app/src/main/java/com/example/android/activitygo/SettingsFragment.java
+++ androidProjects -verdadeiro/app/src/main/java/com/example/android/activitygo/SettingsFragment.java
@@ -7,13 +7,16 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private Button changeProfile;
     private Button changeMedidas;
@@ -21,6 +24,8 @@ public class SettingsFragment extends Fragment {
     private Button calendario;
     private Button contactosImportantes;
     private Button faqs;
+    private Spinner sp;
+    private ArrayAdapter <CharSequence> adapter;
     public SettingsFragment() {
         // Required empty public constructor
     }
@@ -31,6 +36,13 @@ public class SettingsFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_settings_layout, container, false);
         ((MenuPrincipal) getActivity()).getSupportActionBar().setTitle("Definições:");
+
+        sp = (Spinner) v.findViewById(R.id.contactosImportantesSpinner);
+        adapter = ArrayAdapter.createFromResource(getContext(), R.array.contactosImportantes, R.layout.spinner_item_layout);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(this);
+
         Button changeProfile = (Button) v.findViewById(R.id.alterarPerfil);
         changeProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +55,17 @@ public class SettingsFragment extends Fragment {
                 ftChangeProfile.commit();
             }
         });
+
         return v;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
