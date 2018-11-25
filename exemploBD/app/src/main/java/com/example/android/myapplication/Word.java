9e79d7ccce6e80508b32ccea5f043e47f093581c
fc46784 <fc46784@alunos.fc.ul.pt>
2018-11-25 00:52:44 +0000

    tentativa de BD
    base de dados

---
+++ exemploBD/app/src/main/java/com/example/android/myapplication/Word.java
@@ -0,0 +1,28 @@
+package com.example.android.myapplication;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(String mWord) {
+        this.mWord = mWord;
+    }
+
+    public String getWord() {
+        return mWord;
+    }
+
+
+    public void setmWord(@NonNull String mWord) {
+        this.mWord = mWord;
+    }
+}
