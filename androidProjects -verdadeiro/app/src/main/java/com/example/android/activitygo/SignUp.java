541937042a20f7a49020a7c5467bbb8aeda0c2f0
fc46784 <fc46784@alunos.fc.ul.pt>
2018-12-06 23:49:55 +0000

    app a funcar
    app a funcionar

--- androidProjects -verdadeiro/app/src/main/java/com/example/android/activitygo/SignUp.java
+++ androidProjects -verdadeiro/app/src/main/java/com/example/android/activitygo/SignUp.java
@@ -30,10 +30,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
